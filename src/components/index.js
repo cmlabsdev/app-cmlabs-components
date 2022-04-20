a895597e5963613aa295e36b26d027dfeea9df92
bramadl <thenamesbram@gmail.com>
2022-04-20 20:08:09 +0700

    chore: for now hard fix with cm prefix

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,6 @@
 import Vue from "vue";
 import components from "./libs"
 
-let { prefix } = JSON.parse(`<%= JSON.stringify(options) %>`)
-prefix = prefix[0].toUpperCase() + prefix.substring(1);
-
 for (const component in components) {
-  Vue.component(`${prefix}${component}`, components[component]);
+  Vue.component(`Cm${component}`, components[component]);
 }
