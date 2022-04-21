2e19824a5e1b1e79303319e12270c8eb822dfb08
bramadl <thenamesbram@gmail.com>
2022-04-21 09:05:58 +0700

    revert: make component registration uses the default component name (uses cm prefix by default)

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,5 @@ import Vue from "vue";
 import components from "./libs"
 
 for (const component in components) {
-  Vue.component(`Cm${component}`, components[component]);
+  Vue.component(components[component].name, components[component]);
 }
