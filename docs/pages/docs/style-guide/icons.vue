390581864cc3105c59544c6b17f3436016a7e002
Bram Adl <thenamesbram@gmail.com>
2022-05-09 10:46:14 +0700

    docs: new icons page

--- docs/pages/docs/style-guide/icons.vue
+++ docs/pages/docs/style-guide/icons.vue
@@ -1,11 +1,55 @@
 <template>
-  <div>
-    Icons Page
-  </div>
+  <main class="flex flex-col gap-6">
+    <header>
+      <h1 class="font-bold text-h2">Icons</h1>
+      <p class="text-md text-dark-10">Theme and CMLABS icons guidelines.</p>
+    </header>
+
+    <hr class="text-gray-60" />
+
+    <section class="prose prose-ol:p-0 prose-li:p-0 prose-p:m-0 max-w-none">
+      <p class="text-md text-dark-10">
+        We integrate
+        <a class="no-underline hover:underline" href="https://boxicons.com" target="_blank">Boxicons</a>
+        into our icon components library. In order to use
+        <a class="no-underline hover:underline" href="https://boxicons.com" target="_blank">Boxicons</a>,
+        you need to install boxicons through CDN: <br>
+        <code>&lt;link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'></code>
+      </p>
+
+      <h2> Example Usage </h2>
+
+      <p v-highlight>
+        <pre class="language-html"><code>&ltcm-icon icon="bx-user" /&gt;</code>
+<code>&ltcm-icon class="bx-spin" icon="bx-spinner" /&gt;</code>
+<code>&ltcm-icon :class="{ 'rotate-180': isOpen }" icon="bx-chevron-down" /&gt;</code></pre>
+      </p>
+
+      <p>Refer to the official website for more usage</p>
+    </section>
+
+    <footer class="flex items-center justify-between">
+      <nuxt-link
+        class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
+        to="/docs/style-guide/typography"
+      >
+        <cm-icon icon="bx-arrow-back" />
+        <span>Previous</span>
+      </nuxt-link>
+
+      <nuxt-link
+        class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
+        to="/docs/ui-components/accordion"
+      >
+        <span>Continue</span>
+        <cm-icon class="rotate-180" icon="bx-arrow-back" />
+      </nuxt-link>
+    </footer>
+  </main>
 </template>
 
 <script>
 export default {
-  name: "DocsStyleGuideIconsPage"
-}
+  name: "DocsStyleGuideIconsPage",
+};
 </script>
