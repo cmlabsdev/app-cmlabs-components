0f5e9606896fc5af51acf0a78b1a92ff2f004fb4
bramadl <thenamesbram@gmail.com>
2022-07-14 11:49:35 +0700

    chore: setup page layouts and contents markdown

--- docs/pages/docs/ui-components/date-picker.vue
+++ docs/pages/docs/ui-components/date-picker.vue
@@ -1,11 +1,47 @@
 <template>
-  <div>
-    DatePicker Page
-  </div>
+  <main class="flex flex-col gap-6">
+    <header>
+      <h1 class="font-bold text-h2">Date Pickers</h1>
+      <p class="text-md text-dark-10 dark:text-gray-40">
+        Date Pickers component ^^
+      </p>
+    </header>
+
+    <hr class="text-gray-60" />
+
+    <section class="prose prose-ol:p-0 prose-li:p-0 prose-p:m-0 max-w-none">
+      <nuxt-content :document="page" />
+    </section>
+
+    <footer class="flex items-center justify-between">
+      <nuxt-link
+        class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
+        to="/docs/ui-components/data-table"
+      >
+        <cm-icon icon="bx-arrow-back" />
+        <span>Previous</span>
+      </nuxt-link>
+
+      <nuxt-link
+        class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
+        to="/docs/ui-components/date-picker"
+      >
+        <span>Continue</span>
+        <cm-icon class="rotate-180" icon="bx-arrow-back" />
+      </nuxt-link>
+    </footer>
+  </main>
 </template>
 
 <script>
 export default {
-  name: "DocsUiComponentsDatePickerPage"
+  name: "DocsUiComponentsDatePickerPage",
+
+  async asyncData({ $content }) {
+    const page = await $content("date-picker").fetch();
+    return {
+      page,
+    };
+  }
 }
 </script>
--- docs/pages/docs/ui-components/tooltip.vue
+++ docs/pages/docs/ui-components/tooltip.vue
@@ -1,11 +1,47 @@
 <template>
-  <div>
-    Tooltip Page
-  </div>
+  <main class="flex flex-col gap-6">
+    <header>
+      <h1 class="font-bold text-h2">Tooltips</h1>
+      <p class="text-md text-dark-10 dark:text-gray-40">
+        Tooltips component ^^
+      </p>
+    </header>
+
+    <hr class="text-gray-60" />
+
+    <section class="prose prose-ol:p-0 prose-li:p-0 prose-p:m-0 max-w-none">
+      <nuxt-content :document="page" />
+    </section>
+
+    <footer class="flex items-center justify-between">
+      <nuxt-link
+        class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
+        to="/docs/ui-components/toggle"
+      >
+        <cm-icon icon="bx-arrow-back" />
+        <span>Previous</span>
+      </nuxt-link>
+
+      <!-- <nuxt-link
+        class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
+        to="/docs/ui-components/editor"
+      >
+        <span>Continue</span>
+        <cm-icon class="rotate-180" icon="bx-arrow-back" />
+      </nuxt-link> -->
+    </footer>
+  </main>
 </template>
 
 <script>
 export default {
-  name: "DocsUiComponentsTooltipPage"
+  name: "DocsUiComponentsTooltipPage",
+
+  async asyncData({ $content }) {
+    const page = await $content("tooltip").fetch();
+    return {
+      page,
+    };
+  }
 }
 </script>
