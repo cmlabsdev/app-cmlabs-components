6dc7ac93fd654a6c075bb210caddef3deb2b098b
bramadl <thenamesbram@gmail.com>
2022-07-19 16:04:28 +0700

    chore: update modal docs

--- docs/content/modal.md
+++ docs/content/modal.md
@@ -1,67 +1,67 @@
-<p class="text-md text-dark-10 dark:text-gray-40">
-  The avatar component has built-in function to render a pulsing animation only if the <code>src</code> value is falsy (e.g: fetching the picture).
-</p>
+### Code Example
 
-## Basic Usage
+```vue
+<template>
+	<cm-modal v-model="defaultModal">
+		<template #title>
+			<div>Hello World!</div>
+		</template>
 
-When no `src` is passed down, the component will render a circle-pulse animation by default. This is useful if the image is actually being fetched.  
-The component also accept `alt` attribute if you want to specify an alt option.
+		<div>Ini konten ceritanya</div>
+	</cm-modal>
+	<cm-modal v-model="customModal" width="w-[768px]">
+		<template #title>
+			<div>Hello World!</div>
+		</template>
 
-<div style="display: flex; align-items: center; gap: 8px; margin-top: 16px;">
-  <cm-avatar></cm-avatar>
-  <cm-avatar alt="CMLABS" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-</div>
+		<div>Ini konten ceritanya</div>
+	</cm-modal>
+	<cm-modal v-model="noPaddingModal" padding="p-0">
+		<template #title>
+			<div>Hello World!</div>
+		</template>
 
-#### Code Example
+		<div>Ini konten ceritanya</div>
+	</cm-modal>
+	<cm-modal v-model="noClickOutsideModal" :click-to-close="false">
+		<template #title>
+			<div>Hello World!</div>
+			<cm-button @click="noClickOutsideModal = false">
+				<cm-icon icon="bx-x" />
+			</cm-button>
+		</template>
 
-```html
-  <cm-avatar />
-  <cm-avatar alt="CMLABS" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
+		<div>You can't click outside!</div>
+
+		<template #close="{ close }">
+			<cm-button @click="close">
+				<cm-icon icon="bx-x" />
+			</cm-button>
+		</template>
+	</cm-modal>
+
+	<cm-button @click="defaultModal = true"> Open Default Modal </cm-button>
+	<cm-button variant="secondary" @click="customModal = true">
+		Open Custom Width Modal
+	</cm-button>
+	<cm-button variant="info" @click="noPaddingModal = true">
+		Open No Padding Modal
+	</cm-button>
+	<cm-button variant="success" @click="noClickOutsideModal = true">
+		Open No Click Outside Modal
+	</cm-button>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			defaultModal: false,
+			customModal: false,
+			noPaddingModal: false,
+			noClickOutsideModal: false,
+		};
+	},
+};
+</script>
 ```
-
-## Avatar Sizes
-
-The avatar has sizes supported as follow:
-
-- xs
-- sm
-- base -> Default
-- md
-- lg
-- xl
-- unset -> Useful if you want to set the dimension on your own
-
-<div style="display: flex; align-items: center; gap: 8px; margin-top: 16px;">
-  <cm-avatar alt="CMLABS" size="xs" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="sm" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="base" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="md" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="lg" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="xl" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="unset" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" width="80" height="80"></cm-avatar>
-</div>
-
-#### Code Example
-
-```html
-  <cm-avatar alt="CMLABS" size="xs" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="sm" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="base" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="md" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="lg" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="xl" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="unset" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" width="80" height="80" />
-```
-
-## Square Avatar
-
-The avatar by default will render a circle shape. To force the avatar to not be circle, use the `rounded` boolean props and set it to false.
-
-<cm-avatar alt="CMLABS" :rounded="false" size="lg" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-
-#### Code Example
-
-```html
-  <cm-avatar alt="CMLABS" :rounded="false" size="lg" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-```
-
--- docs/pages/docs/ui-components/modal.vue
+++ docs/pages/docs/ui-components/modal.vue
@@ -1,47 +1,114 @@
 <template>
-  <main class="flex flex-col gap-6">
-    <header>
-      <h1 class="font-bold text-h2">Modals</h1>
-      <p class="text-md text-dark-10 dark:text-gray-40">
-        Modals component ^^
-      </p>
-    </header>
+	<main class="flex flex-col gap-6">
+		<header>
+			<h1 class="font-bold text-h2">Modals</h1>
+			<p class="text-md text-dark-10 dark:text-gray-40">Modals component ^^</p>
+		</header>
 
-    <hr class="text-gray-60" />
+		<hr class="text-gray-60" />
 
-    <section class="prose prose-ol:p-0 prose-li:p-0 prose-p:m-0 prose-table:my-0 max-w-none">
-      <nuxt-content :document="page" />
-    </section>
+		<section
+			class="prose prose-ol:p-0 prose-li:p-0 prose-p:m-0 prose-table:my-0 max-w-none"
+		>
+			<p class="text-md text-dark-10 dark:text-gray-40">
+				Lorem ipsum odor amet, consectetuer adipiscing elit. Mauris massa quam
+				pretium venenatis at quis interdum eros. Laoreet efficitur nullam
+				himenaeos nam pellentesque pretium turpis facilisi dis. Eu quis dictum
+				aenean consequat nibh blandit vivamus. Vulputate venenatis varius eros
+				facilisis id. Rutrum platea suspendisse a ipsum sit pharetra adipiscing.
+				Sem vitae etiam maximus venenatis eleifend cubilia fermentum
+				consectetur. Leo erat tincidunt facilisis ultrices vivamus justo
+				dignissim.
+			</p>
 
-    <footer class="flex items-center justify-between">
-      <nuxt-link
-        class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
-        to="/docs/ui-components/forms"
-      >
-        <cm-icon icon="bx-arrow-back" />
-        <span>Previous</span>
-      </nuxt-link>
+			<h2>Basic Usage</h2>
 
-      <nuxt-link
-        class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
-        to="/docs/ui-components/pagination"
-      >
-        <span>Continue</span>
-        <cm-icon class="rotate-180" icon="bx-arrow-back" />
-      </nuxt-link>
-    </footer>
-  </main>
+			<div class="flex flex-col gap-4 mt-4 p-4 bg-white rounded shadow">
+				<cm-modal v-model="defaultModal">
+					<template #title>
+						<div>Hello World!</div>
+					</template>
+
+					<div>Ini konten ceritanya</div>
+				</cm-modal>
+				<cm-modal v-model="customModal" width="w-[768px]">
+					<template #title>
+						<div>Hello World!</div>
+					</template>
+
+					<div>Ini konten ceritanya</div>
+				</cm-modal>
+				<cm-modal v-model="noPaddingModal" padding="p-0">
+					<template #title>
+						<div>Hello World!</div>
+					</template>
+
+					<div>Ini konten ceritanya</div>
+				</cm-modal>
+				<cm-modal v-model="noClickOutsideModal" :click-to-close="false">
+					<template #title>
+						<div>Hello World!</div>
+            <cm-button @click="noClickOutsideModal = false">
+              <cm-icon icon="bx-x" />
+            </cm-button>
+					</template>
+
+					<div>You can't click outside!</div>
+
+          <template #close="{ close }">
+            <cm-button @click="close">
+              <cm-icon icon="bx-x" />
+            </cm-button>
+          </template>
+				</cm-modal>
+
+				<cm-button @click="defaultModal = true"> Open Default Modal </cm-button>
+				<cm-button variant="secondary" @click="customModal = true"> Open Custom Width Modal </cm-button>
+				<cm-button variant="info" @click="noPaddingModal = true"> Open No Padding Modal </cm-button>
+				<cm-button variant="success" @click="noClickOutsideModal = true"> Open No Click Outside Modal </cm-button>
+			</div>
+
+			<nuxt-content :document="page" />
+		</section>
+
+		<footer class="flex items-center justify-between">
+			<nuxt-link
+				class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
+				to="/docs/ui-components/forms"
+			>
+				<cm-icon icon="bx-arrow-back" />
+				<span>Previous</span>
+			</nuxt-link>
+
+			<nuxt-link
+				class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
+				to="/docs/ui-components/pagination"
+			>
+				<span>Continue</span>
+				<cm-icon class="rotate-180" icon="bx-arrow-back" />
+			</nuxt-link>
+		</footer>
+	</main>
 </template>
 
 <script>
 export default {
-  name: "DocsUiComponentsModalPage",
+	name: "DocsUiComponentsModalPage",
 
-  async asyncData({ $content }) {
-    const page = await $content("modal").fetch();
-    return {
-      page,
-    };
-  }
-}
+	async asyncData({ $content }) {
+		const page = await $content("modal").fetch();
+		return {
+			page,
+		};
+	},
+
+	data() {
+		return {
+			defaultModal: false,
+      customModal: false,
+      noPaddingModal: false,
+      noClickOutsideModal: false,
+		};
+	},
+};
 </script>
