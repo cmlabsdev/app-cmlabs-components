313853025011c234ebe77976e55744d2750d612c
bramadl <thenamesbram@gmail.com>
2022-07-19 16:46:28 +0700

    chore: update forms doc

--- docs/content/forms.md
+++ docs/content/forms.md
@@ -1,67 +1,26 @@
-<p class="text-md text-dark-10 dark:text-gray-40">
-  The avatar component has built-in function to render a pulsing animation only if the <code>src</code> value is falsy (e.g: fetching the picture).
-</p>
+### Code Example
 
-## Basic Usage
+```vue
+<template>
+	<cm-form-group>
+		<template #label>
+			<cm-form-label> Username </cm-form-label>
+		</template>
 
-When no `src` is passed down, the component will render a circle-pulse animation by default. This is useful if the image is actually being fetched.  
-The component also accept `alt` attribute if you want to specify an alt option.
+		<cm-form-control>
+			<cm-input placeholder="Enter your username" />
+		</cm-form-control>
 
-<div style="display: flex; align-items: center; gap: 8px; margin-top: 16px;">
-  <cm-avatar></cm-avatar>
-  <cm-avatar alt="CMLABS" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-</div>
+		<template #error>
+			<cm-form-error> Please insert your username! </cm-form-error>
+		</template>
 
-#### Code Example
-
-```html
-  <cm-avatar />
-  <cm-avatar alt="CMLABS" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
+		<template #utils>
+			<span class="text-dark-40 text-sm"
+				>We will create a unique ID based on your username that can't never be
+				changed.</span
+			>
+		</template>
+	</cm-form-group>
+</template>
 ```
-
-## Avatar Sizes
-
-The avatar has sizes supported as follow:
-
-- xs
-- sm
-- base -> Default
-- md
-- lg
-- xl
-- unset -> Useful if you want to set the dimension on your own
-
-<div style="display: flex; align-items: center; gap: 8px; margin-top: 16px;">
-  <cm-avatar alt="CMLABS" size="xs" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="sm" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="base" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="md" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="lg" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="xl" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="unset" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" width="80" height="80"></cm-avatar>
-</div>
-
-#### Code Example
-
-```html
-  <cm-avatar alt="CMLABS" size="xs" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="sm" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="base" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="md" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="lg" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="xl" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="unset" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" width="80" height="80" />
-```
-
-## Square Avatar
-
-The avatar by default will render a circle shape. To force the avatar to not be circle, use the `rounded` boolean props and set it to false.
-
-<cm-avatar alt="CMLABS" :rounded="false" size="lg" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-
-#### Code Example
-
-```html
-  <cm-avatar alt="CMLABS" :rounded="false" size="lg" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-```
-
--- docs/pages/docs/ui-components/forms.vue
+++ docs/pages/docs/ui-components/forms.vue
@@ -1,47 +1,88 @@
 <template>
-  <main class="flex flex-col gap-6">
-    <header>
-      <h1 class="font-bold text-h2">Forms</h1>
-      <p class="text-md text-dark-10 dark:text-gray-40">
-        Forms component ^^
+	<main class="flex flex-col gap-6">
+		<header>
+			<h1 class="font-bold text-h2">Forms</h1>
+			<p class="text-md text-dark-10 dark:text-gray-40">Forms component ^^</p>
+		</header>
+
+		<hr class="text-gray-60" />
+
+		<section
+			class="prose prose-ol:p-0 prose-li:p-0 prose-p:m-0 prose-table:my-0 max-w-none"
+		>
+			<p class="text-md text-dark-10 dark:text-gray-40">
+				Use the <code>cm-form-group</code> component to group
+				<strong>form control</strong>, <strong>form error</strong>, and
+				<strong>form label</strong>. You may use <code>cm-input</code> as a
+				child within the <strong>form control</strong> component. This will
+				allow you to make a form component. See example below
+			</p>
+
+			<h2>Basic Usage</h2>
+
+      <p>
+        The form group component exposes 4 slots that you can use one and another.
+        The following example shows full usage of the slots starting from the label,
+        form control wrapper for input, error field, and utils used to display other
+        information. You can combine the usage of <code>v-if</code> with the error
+        slots to dynamically show the error element.
       </p>
-    </header>
 
-    <hr class="text-gray-60" />
+			<div class="flex flex-col gap-4 mt-4 p-4 bg-white rounded shadow">
+				<cm-form-group>
+					<template #label>
+						<cm-form-label> Username </cm-form-label>
+					</template>
 
-    <section class="prose prose-ol:p-0 prose-li:p-0 prose-p:m-0 prose-table:my-0 max-w-none">
-      <nuxt-content :document="page" />
-    </section>
+					<cm-form-control>
+						<cm-input placeholder="Enter your username" />
+					</cm-form-control>
 
-    <footer class="flex items-center justify-between">
-      <nuxt-link
-        class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
-        to="/docs/ui-components/editor"
-      >
-        <cm-icon icon="bx-arrow-back" />
-        <span>Previous</span>
-      </nuxt-link>
+					<template #error>
+						<cm-form-error> Please insert your username! </cm-form-error>
+					</template>
 
-      <nuxt-link
-        class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
-        to="/docs/ui-components/modal"
-      >
-        <span>Continue</span>
-        <cm-icon class="rotate-180" icon="bx-arrow-back" />
-      </nuxt-link>
-    </footer>
-  </main>
+					<template #utils>
+						<span class="text-dark-40 text-sm"
+							>We will create a unique ID based on your username that can't
+							never be changed.</span
+						>
+					</template>
+				</cm-form-group>
+			</div>
+
+			<nuxt-content :document="page" />
+		</section>
+
+		<footer class="flex items-center justify-between">
+			<nuxt-link
+				class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
+				to="/docs/ui-components/editor"
+			>
+				<cm-icon icon="bx-arrow-back" />
+				<span>Previous</span>
+			</nuxt-link>
+
+			<nuxt-link
+				class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
+				to="/docs/ui-components/modal"
+			>
+				<span>Continue</span>
+				<cm-icon class="rotate-180" icon="bx-arrow-back" />
+			</nuxt-link>
+		</footer>
+	</main>
 </template>
 
 <script>
 export default {
-  name: "DocsUiComponentsFormPage",
+	name: "DocsUiComponentsFormPage",
 
-  async asyncData({ $content }) {
-    const page = await $content("dropdown").fetch();
-    return {
-      page,
-    };
-  }
-}
+	async asyncData({ $content }) {
+		const page = await $content("forms").fetch();
+		return {
+			page,
+		};
+	},
+};
 </script>
