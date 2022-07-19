07e48beb668f274ceaa96781c6f9d66c472f7568
bramadl <thenamesbram@gmail.com>
2022-07-19 09:39:30 +0700

    chore: update toast docs

--- docs/content/toast.md
+++ docs/content/toast.md
@@ -1,67 +1,67 @@
-<p class="text-md text-dark-10 dark:text-gray-40">
-  The avatar component has built-in function to render a pulsing animation only if the <code>src</code> value is falsy (e.g: fetching the picture).
-</p>
-
-## Basic Usage
-
-When no `src` is passed down, the component will render a circle-pulse animation by default. This is useful if the image is actually being fetched.  
-The component also accept `alt` attribute if you want to specify an alt option.
-
-<div style="display: flex; align-items: center; gap: 8px; margin-top: 16px;">
-  <cm-avatar></cm-avatar>
-  <cm-avatar alt="CMLABS" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-</div>
-
 #### Code Example
 
-```html
-  <cm-avatar />
-  <cm-avatar alt="CMLABS" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
+```vue
+<cm-toast />
+
+<cm-button variant="primary" @click="$cmToast">Toggle Toast</cm-button>
+
+<!-- available variants are only "primary", "info", "success", "warning", "danger" -->
+<cm-button
+	variant="success"
+	@click="
+		$cmToast(
+			{ variant: 'success', title: 'Welcome!', text: 'Hello World!' },
+			4000
+		)
+	"
+>Toggle Toast</cm-button>
+
+<cm-button
+	variant="warning"
+	@click="
+		$cmToast(
+			{
+				variant: 'warning',
+				title: 'This toast has a link!',
+				text: 'Check the link below',
+				hasLink: true,
+				toLink: 'https://youtube.com',
+				textLink: 'Go to youtube!',
+			},
+			6000
+		)
+	"
+>Toggle Toast</cm-button>
+
+<cm-button
+	variant="info"
+	@click="
+		$cmToast(
+			{
+				variant: 'info',
+				title: 'This toast has a link!',
+				text: '<strong>It can render an element!</strong> hehehehe',
+				hasLink: true,
+				toLink: 'https://youtube.com',
+				textLink: 'Go to youtube!',
+			},
+			6000
+		)
+	"
+>Toggle Toast</cm-button>
+
+<cm-button
+	variant="danger"
+	@click="
+		$cmToast(
+			{
+				variant: 'danger',
+				title: 'Custom Icon',
+				text: 'The icon must use the full bx- class form',
+				icon: 'bx-user',
+			},
+			6000
+		)
+	"
+>Toggle Toast</cm-button>
 ```
-
-## Avatar Sizes
-
-The avatar has sizes supported as follow:
-
-- xs
-- sm
-- base -> Default
-- md
-- lg
-- xl
-- unset -> Useful if you want to set the dimension on your own
-
-<div style="display: flex; align-items: center; gap: 8px; margin-top: 16px;">
-  <cm-avatar alt="CMLABS" size="xs" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="sm" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="base" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="md" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="lg" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="xl" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-  <cm-avatar alt="CMLABS" size="unset" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" width="80" height="80"></cm-avatar>
-</div>
-
-#### Code Example
-
-```html
-  <cm-avatar alt="CMLABS" size="xs" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="sm" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="base" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="md" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="lg" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="xl" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-  <cm-avatar alt="CMLABS" size="unset" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" width="80" height="80" />
-```
-
-## Square Avatar
-
-The avatar by default will render a circle shape. To force the avatar to not be circle, use the `rounded` boolean props and set it to false.
-
-<cm-avatar alt="CMLABS" :rounded="false" size="lg" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg"></cm-avatar>
-
-#### Code Example
-
-```html
-  <cm-avatar alt="CMLABS" :rounded="false" size="lg" src="https://design.cmlabs.dev/_nuxt/img/cmlabs.21b68bc.svg" />
-```
-
--- docs/pages/docs/ui-components/toast.vue
+++ docs/pages/docs/ui-components/toast.vue
@@ -1,47 +1,129 @@
 <template>
-  <main class="flex flex-col gap-6">
-    <header>
-      <h1 class="font-bold text-h2">Toasts</h1>
-      <p class="text-md text-dark-10 dark:text-gray-40">
-        Toasts component ^^
-      </p>
-    </header>
+	<main class="flex flex-col gap-6">
+		<header>
+			<h1 class="font-bold text-h2">Toasts</h1>
+			<p class="text-md text-dark-10 dark:text-gray-40">Toasts component ^^</p>
+		</header>
 
-    <hr class="text-gray-60" />
+		<hr class="text-gray-60" />
 
-    <section class="prose prose-ol:p-0 prose-li:p-0 prose-p:m-0 max-w-none">
-      <nuxt-content :document="page" />
-    </section>
+		<section class="prose prose-ol:p-0 prose-li:p-0 prose-p:m-0 max-w-none">
+			<p class="text-md text-dark-10 dark:text-gray-40">
+				Lorem ipsum odor amet, consectetuer adipiscing elit. Mauris massa quam
+				pretium venenatis at quis interdum eros. Laoreet efficitur nullam
+				himenaeos nam pellentesque pretium turpis facilisi dis. Eu quis dictum
+				aenean consequat nibh blandit vivamus. Vulputate venenatis varius eros
+				facilisis id. Rutrum platea suspendisse a ipsum sit pharetra adipiscing.
+				Sem vitae etiam maximus venenatis eleifend cubilia fermentum
+				consectetur. Leo erat tincidunt facilisis ultrices vivamus justo
+				dignissim.
+			</p>
 
-    <footer class="flex items-center justify-between">
-      <nuxt-link
-        class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
-        to="/docs/ui-components/table"
-      >
-        <cm-icon icon="bx-arrow-back" />
-        <span>Previous</span>
-      </nuxt-link>
+			<h2>Basic Usage</h2>
 
-      <nuxt-link
-        class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
-        to="/docs/ui-components/toggle"
-      >
-        <span>Continue</span>
-        <cm-icon class="rotate-180" icon="bx-arrow-back" />
-      </nuxt-link>
-    </footer>
-  </main>
+			<p class="text-md">
+				Use the <code>$cmToast()</code> helper passing in the toast options and duration as the args. <br>
+				See below example to see what are the available toast options.
+			</p>
+
+			<div
+				class="flex flex-col items-start gap-4 mt-4 bg-white shadow rounded p-4"
+			>
+				<cm-toast />
+				<cm-button variant="primary" @click="$cmToast">Toggle Toast</cm-button>
+				<cm-button
+					variant="success"
+					@click="
+						$cmToast(
+							{ variant: 'success', title: 'Welcome!', text: 'Hello World!' },
+							4000
+						)
+					"
+					>Toggle Toast</cm-button
+				>
+				<cm-button
+					variant="warning"
+					@click="
+						$cmToast(
+							{
+								variant: 'warning',
+								title: 'This toast has a link!',
+								text: 'Check the link below',
+								hasLink: true,
+								toLink: 'https://youtube.com',
+								textLink: 'Go to youtube!',
+							},
+							6000
+						)
+					"
+					>Toggle Toast</cm-button
+				>
+				<cm-button
+					variant="info"
+					@click="
+						$cmToast(
+							{
+								variant: 'info',
+								title: 'This toast has a link!',
+								text: '<strong>It can render an element!</strong> hehehehe',
+								hasLink: true,
+								toLink: 'https://youtube.com',
+								textLink: 'Go to youtube!',
+							},
+							6000
+						)
+					"
+					>Toggle Toast</cm-button
+				>
+				<cm-button
+					variant="danger"
+					@click="
+						$cmToast(
+							{
+								variant: 'danger',
+								title: 'Custom Icon',
+								text: 'The icon must use the full bx- class form',
+								icon: 'bx-user'
+							},
+							6000
+						)
+					"
+					>Toggle Toast</cm-button
+				>
+			</div>
+
+			<nuxt-content :document="basicExample" />
+		</section>
+
+		<footer class="flex items-center justify-between">
+			<nuxt-link
+				class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
+				to="/docs/ui-components/table"
+			>
+				<cm-icon icon="bx-arrow-back" />
+				<span>Previous</span>
+			</nuxt-link>
+
+			<nuxt-link
+				class="flex items-center gap-2 py-3 px-4 rounded border border-dark-10 hover:border-dark-60"
+				to="/docs/ui-components/toggle"
+			>
+				<span>Continue</span>
+				<cm-icon class="rotate-180" icon="bx-arrow-back" />
+			</nuxt-link>
+		</footer>
+	</main>
 </template>
 
 <script>
 export default {
-  name: "DocsUiComponentsToastPage",
+	name: "DocsUiComponentsToastPage",
 
-  async asyncData({ $content }) {
-    const page = await $content("toast").fetch();
-    return {
-      page,
-    };
-  }
-}
+	async asyncData({ $content }) {
+		const basicExample = await $content("toast").fetch();
+		return {
+			basicExample,
+		};
+	},
+};
 </script>
