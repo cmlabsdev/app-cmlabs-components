4585ef8d570484d2baa0beb5b7228743901dc833
Bram Adl <thenamesbram@gmail.com>
2022-08-10 10:14:48 +0700

    fix: data table pagination

--- src/components/libs/DataTable.vue
+++ src/components/libs/DataTable.vue
@@ -3,10 +3,7 @@
     <slot name="header" />
 
     <div v-if="$fetchState.pending">
-      <cm-table
-        :table-columns="tableOptions.columns"
-        :table-data="[]"
-      >
+      <cm-table :table-columns="tableOptions.columns" :table-data="[]">
         <template #skeleton>
           <cm-table-skeleton
             v-for="n in 4"
@@ -16,14 +13,9 @@
         </template>
       </cm-table>
     </div>
-    <div v-else-if="$fetchState.error">
-      An error has occured...
-    </div>
+    <div v-else-if="$fetchState.error">An error has occured...</div>
     <div v-else>
-      <slot
-        :table-columns="tableColumns"
-        :table-data="tableData"
-      />
+      <slot :table-columns="tableColumns" :table-data="tableData" />
     </div>
 
     <slot
@@ -35,19 +27,18 @@
     >
       <cm-data-table-footer>
         <cm-data-table-count>
-          Showing <strong>{{ currentCount }}</strong> of <strong>{{ totalCount }}</strong>
+          Showing <strong>{{ currentCount }}</strong> of
+          <strong>{{ totalCount }}</strong>
         </cm-data-table-count>
 
         <div class="flex items-center gap-8">
           <div v-if="!noPerPage" class="flex items-center gap-4">
-            <p class="text-sm">
-              Rows per page
-            </p>
+            <p class="text-sm">Rows per page</p>
             <cm-select
               class="!w-20"
               :options="rowsPerPageOptions"
               :value="perPage"
-              @update:value="value => $emit('change:per-page', value)"
+              @update:value="(value) => $emit('change:per-page', value)"
             />
           </div>
 
@@ -55,32 +46,52 @@
             <template #prev>
               <cm-pagination-button
                 :is-disabled="+currentPage === 1"
-                @click="$emit('go:to', (+currentPage - 1))"
+                @click="handleClick(+currentPage - 1)"
               >
                 «
               </cm-pagination-button>
             </template>
 
-            <template v-if="totalPage < 4">
+            <template v-if="totalPage <= threshold">
               <cm-pagination-button
                 v-for="page in Number(totalPage)"
                 :key="page"
                 :is-active="page === Number(currentPage)"
                 square
-                @click="$emit('go:to', (+page))"
+                @click="$emit('go:to', +page)"
               >
                 {{ page }}
               </cm-pagination-button>
             </template>
 
             <template v-else>
-              
+              <cm-pagination-button
+                v-if="showPrevDots"
+                @click="jumpOutbounds('down')"
+                >...</cm-pagination-button
+              >
+
+              <cm-pagination-button
+                v-for="page in paginationOutbounds"
+                :key="page"
+                :is-active="page === +currentPage"
+                square
+                @click="() => $emit('go:to', +page)"
+              >
+                {{ page }}
+              </cm-pagination-button>
+
+              <cm-pagination-button
+                v-if="showNextDots"
+                @click="jumpOutbounds('up')"
+                >...</cm-pagination-button
+              >
             </template>
 
             <template #next>
               <cm-pagination-button
                 :is-disabled="!+totalPage || totalPage === currentPage"
-                @click="$emit('go:to', (+currentPage + 1))"
+                @click="handleClick(+currentPage + 1)"
               >
                 »
               </cm-pagination-button>
@@ -94,7 +105,7 @@
 
 <script>
 export default {
-  name: 'CmDataTable',
+  name: "CmDataTable",
 
   props: {
     /**
@@ -103,7 +114,7 @@ export default {
      */
     apiEndpoint: {
       type: String,
-      default: null
+      default: null,
     },
     /**
      * Which property should be used as a key
@@ -121,7 +132,7 @@ export default {
      */
     dataKey: {
       type: String,
-      default: null
+      default: null,
     },
     /**
      * The table data formatter. Used to modify
@@ -130,9 +141,9 @@ export default {
      */
     formatData: {
       type: Function,
-      default (data) {
-        return data
-      }
+      default(data) {
+        return data;
+      },
     },
     /**
      * Which store should be set upon successful
@@ -141,14 +152,14 @@ export default {
      */
     populateStore: {
       type: String,
-      default: null
+      default: null,
     },
     /**
      * Disable per page functionality if set to true.
      */
     noPerPage: {
       type: Boolean,
-      default: false
+      default: false,
     },
     /**
      * The table data options containing columns,
@@ -174,41 +185,38 @@ export default {
      */
     tableOptions: {
       type: Object,
-      default () {
+      default() {
         return {
-          columns: [
-            'column1',
-            'column2',
-            'column3',
-            'action'
-          ],
+          columns: ["column1", "column2", "column3", "action"],
           data: [
             {
               id: 0,
-              column1: 'Column 1A Value',
-              column2: 'Column 2A Value',
-              column3: 'Column 3A Value'
+              column1: "Column 1A Value",
+              column2: "Column 2A Value",
+              column3: "Column 3A Value",
             },
             {
               id: 1,
-              column1: 'Column 1B Value',
-              column2: 'Column 2B Value',
-              column3: 'Column 3B Value'
-            }
+              column1: "Column 1B Value",
+              column2: "Column 2B Value",
+              column3: "Column 3B Value",
+            },
           ],
           pagination: {
-            per_page: '8',
-            current_page: '1',
-            total_page: '1',
-            total_items: '8'
-          }
-        }
-      }
-    }
+            per_page: "8",
+            current_page: "1",
+            total_page: "1",
+            total_items: "8",
+          },
+        };
+      },
+    },
   },
 
-  data () {
+  data() {
     return {
+      threshold: 3,
+      paginationOutbounds: [1, 2, 3, 4],
       /**
        * Replica of the tableOption property.
        * This object is used to avoid mutating original data.
@@ -217,8 +225,13 @@ export default {
       /**
        * Items to show for the rows per page data.
        */
-      rowsPerPageOptions: [{ label: '5' }, { label: '10' }, { label: '15' }, { label: '20' }]
-    }
+      rowsPerPageOptions: [
+        { label: "5" },
+        { label: "10" },
+        { label: "15" },
+        { label: "20" },
+      ],
+    };
   },
 
   /**
@@ -239,45 +252,64 @@ export default {
    * Finally, if populateStore is defined, call the vuex mutations and send
    * the data taken from the API call.
    */
-  async fetch () {
-    if (!this.apiEndpoint) { return }
+  async fetch() {
+    if (!this.apiEndpoint) {
+      return;
+    }
 
-    const tableOptions = Object.assign({}, this.tableOptions)
-    const { data, pagination } = await this.$axios.$get(this.apiEndpoint) || {}
+    const tableOptions = Object.assign({}, this.tableOptions);
+    const { data, pagination } =
+      (await this.$axios.$get(this.apiEndpoint)) || {};
 
-    const dataValue = this.dataKey ? data[this.dataKey] : data
+    const dataValue = this.dataKey ? data[this.dataKey] : data;
 
-    tableOptions.data = this.formatData(dataValue)
-    tableOptions.pagination = pagination
-    this.tableOptions_ = tableOptions
+    tableOptions.data = this.formatData(dataValue);
+    tableOptions.pagination = pagination;
+    this.tableOptions_ = tableOptions;
 
-    if (this.populateStore) { this.$store.commit(this.populateStore, dataValue) }
+    if (this.populateStore) {
+      this.$store.commit(this.populateStore, dataValue);
+    }
   },
 
   computed: {
+    showPrevDots() {
+      if (this.paginationOutbounds.includes(1)) return false;
+      return true;
+    },
+    showNextDots() {
+      if (this.paginationOutbounds.includes(this.totalPage)) return false;
+      return true;
+    },
     /**
      * The columns from the tableOptions replica.
      * Defaults to the original tableOptions.
      */
-    tableColumns () {
-      if (this.tableOptions_) { return this.tableOptions_.columns }
-      return this.tableOptions.columns ? [...this.tableOptions.columns] : []
+    tableColumns() {
+      if (this.tableOptions_) {
+        return this.tableOptions_.columns;
+      }
+      return this.tableOptions.columns ? [...this.tableOptions.columns] : [];
     },
     /**
      * The data from the tableOptions replica.
      * Defaults to the original tableOptions.
      */
-    tableData () {
-      if (this.tableOptions_) { return this.tableOptions_.data }
-      return this.tableOptions.data ? [...this.tableOptions.data] : []
+    tableData() {
+      if (this.tableOptions_) {
+        return this.tableOptions_.data;
+      }
+      return this.tableOptions.data ? [...this.tableOptions.data] : [];
     },
     /**
      * The pagination from the tableOptions replica.
      * Defaults to the original tableOptions.
      */
-    pagination () {
-      if (this.tableOptions_) { return this.tableOptions_.pagination }
-      return this.tableOptions.pagination ? this.tableOptions.pagination : {}
+    pagination() {
+      if (this.tableOptions_) {
+        return this.tableOptions_.pagination;
+      }
+      return this.tableOptions.pagination ? this.tableOptions.pagination : {};
     },
     /**
      * The amount of records inside the table data items to be shown
@@ -285,62 +317,97 @@ export default {
      * Recalculate automatically when searching,
      * filtering, or no data because tableData is re-assigned upon fetch hooks.
      */
-    currentCount () {
-      return this.tableData.length || 0
+    currentCount() {
+      return this.tableData.length || 0;
     },
     /**
      * The total amount of the original data taken from the pagination.
      * Automatically synced with the server response.
      */
-    totalCount () {
-      return this.pagination.total_items || 0
+    totalCount() {
+      return this.pagination.total_items || 0;
     },
     /**
      * Current per page from the pagination.
      * Automatically synced with the server response.
      */
-    perPage () {
-      return this.pagination.per_page || 10
+    perPage() {
+      return this.pagination.per_page || 10;
     },
     /**
      * Current page from the pagination.
      * Automatically synced with the server response.
      */
-    currentPage () {
-      return this.pagination.current_page || 0
+    currentPage() {
+      return this.pagination.current_page || 0;
     },
     /**
      * Total page from the pagination.
      * Automatically synced with the server response.
      */
-    totalPage () {
-      return this.pagination.total_page || 0
-    }
+    totalPage() {
+      return this.pagination.total_page || 0;
+    },
   },
 
   watch: {
     /**
      * When API Endpoint changes, re-fetch the data.
      */
-    apiEndpoint () {
-      this.$fetch()
-    }
+    apiEndpoint() {
+      this.$fetch();
+    },
   },
 
-  mounted () {
+  mounted() {
     /**
      * Emitter to handle refreshing data table.
      * Automatically re-fetch the data.
      */
-    this.$nuxt.$on("refresh:data_table", () => { this.$fetch() })
+    this.$nuxt.$on("refresh:data_table", () => {
+      this.$fetch();
+    });
   },
 
-  beforeDestory () {
+  beforeDestory() {
     /**
      * Emitter to handle refreshing data table.
      * De-attach the listener for performance.
      */
-    this.$nuxt.$off("refresh:data_table", () => { this.$fetch() })
+    this.$nuxt.$off("refresh:data_table", () => {
+      this.$fetch();
+    });
+  },
+
+  methods: {
+    handleClick (page) {
+      const direction = +page < +this.currentPage ? "down" : "up"
+
+      if (+page !== +this.totalPage) {
+        const isPageBreaker = this.isPageBreaker(+page)
+        if (isPageBreaker) {
+          this.jumpOutbounds(direction)
+        }
+      }
+
+      this.$emit('go:to', +page)
+    },
+    isPageBreaker(page) {
+      if (page === 1) return false;
+      return (page % this.threshold) - 1 === 0;
+    },
+    jumpOutbounds(direction) {
+      this.paginationOutbounds = this.paginationOutbounds.map((outbound) => {
+        if (direction === "down") {
+          this.currentPage = this.paginationOutbounds[0];
+          return outbound - this.threshold;
+        } else {
+          this.currentPage =
+            this.paginationOutbounds[this.paginationOutbounds.length - 1];
+          return outbound + this.threshold;
+        }
+      });
+    },
   }
-}
+};
 </script>
